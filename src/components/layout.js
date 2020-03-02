b7a4924d0ba71846c0d7511d0921b2eb866d1ff1
Rochdi <roch2di@gmail.com>
2020-03-02 14:10:24 +0100

    Prettier code formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,43 @@
-import React from 'react'
-import searchIcon from "../assets/icons/SearchIcon.svg"
-import cartIcon from "../assets/icons/ShoppingCartIcon.svg"
+import React from "react";
+import searchIcon from "../assets/icons/SearchIcon.svg";
+import cartIcon from "../assets/icons/ShoppingCartIcon.svg";
 
 const Layout = props => {
-return (
-	<React.Fragment>
-	<NavBar />
-	{props.children}
-	<Footer />
-	</React.Fragment>
- )
-}
+  return (
+    <React.Fragment>
+      <NavBar />
+      {props.children}
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-export default Layout
+export default Layout;
 
 const NavBar = () => {
- return (
-	<React.Fragment>
-	<div className='head--msg'>
-	FREE SHIPPING ON ALL ORDERS OVER $150.
-	</div>
-	<nav className='nav'>
-	<div className='nav--Logo'>
-	Logo
-	</div>
-	<ul className='nav--items'>
-		<li>Categorie</li>
-		<li>Categorie</li>
-		<li>Categorie</li>
-	</ul>
-	<div className='nav--icons'>
-	<button>
-		<img src={searchIcon}/>
-	</button>
-	<button>
-		<img src={cartIcon}/>
-	</button>
-	</div>
-	</nav>
-	</React.Fragment>
-	)
-}
+  return (
+    <React.Fragment>
+      <div className="head--msg">FREE SHIPPING ON ALL ORDERS OVER $450.</div>
+      <nav className="nav">
+        <div className="nav--Logo">Logo</div>
+        <ul className="nav--items">
+          <li>Categorie</li>
+          <li>Categorie</li>
+          <li>Categorie</li>
+        </ul>
+        <div className="nav--icons">
+          <button>
+            <img src={searchIcon} />
+          </button>
+          <button>
+            <img src={cartIcon} />
+          </button>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
 
 const Footer = () => {
-  return (
-	<div className="footer">
-	footer goes here
-	</div>
-	)
-}
+  return <div className="footer">footer goes here</div>;
+};
