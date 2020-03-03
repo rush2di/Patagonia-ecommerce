46b239e1c08cd4712825ca165e0be3179e669704
Rochdi <roch2di@gmail.com>
2020-03-03 21:36:44 +0100

    --Update-styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import heroBg from "../assets/images/Hero-min.jpg";
 
 const Home = props => {
-  return <div>home</div>;
+  return (
+    <React.Fragment>
+      <div className="main--hero" style={{ backgroundImage: `url(${heroBg})` }}>
+        <div className="main--hero-over">
+          <div className="container">
+            <div className="main--hero-over-txt">
+              <h1>
+                WE HAVE
+                <br />
+                THE WORKS
+              </h1>
+              <p>ITS ALL FUN AND GAMES UNTIL YOU CATCH TOO MUCH AIR</p>
+              <button>DISCOVER SOFTECH</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
