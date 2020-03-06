7c748ad0dbf034e955c44db9adc22fcfe8842134
Rochdi <roch2di@gmail.com>
2020-03-06 01:10:26 +0100

    --Finished-Sign-In-Page

---
+++ src/components/form.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const Formy = ({ errors, touched, isSubmitting }) => {
+  return (
+    <Form className="form">
+      <label>EMAIL</label>
+      <Field type="email" name="email" />
+      <div>{touched.email && errors.email && <p>{errors.email}</p>}</div>
+      <label>PASSWORD</label>
+      <Field type="password" name="password" />
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+      </div>
+      <a href="/#">FORGOT YOUR PASSWORD?</a>
+      <label>
+        <Field type="checkbox" name="remember" />
+        Remember Me
+      </label>
+      <button disabled={isSubmitting} type="submit">
+        Sign In
+      </button>
+    </Form>
+  );
+};
+
+const mapPropsToValues = ({ name, email, remember }) => {
+  return {
+    name: name || "",
+    email: email || "",
+    remember: remember || false
+  };
+};
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+  remember: Yup.boolean()
+});
+
+const handleSubmit = (values, { resetForm, setSubmitting }) => {
+  setTimeout(() => {
+    resetForm();
+    setSubmitting(false);
+  }, 1500);
+};
+
+export default withFormik({
+  mapPropsToValues,
+  validationSchema,
+  handleSubmit
+})(Formy);
--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import Formy from "./form";
 
 const SignIn = () => {
   return (
@@ -34,51 +34,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-const Formy = props => {
-  const formValidate = values => {
-    const errors = {};
-    const regexp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    if (!values.email) {
-      errors.email = "Please fill in the email field";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Invalid email adress";
-    }
-    return errors;
-  };
-  const handleSubmit = (values, setSubmitting) => {
-    console.log("submited");
-  };
-
-  return (
-    <Formik
-      initalValues={{ email: "", password: "" }}
-      validate={values => formValidate(values)}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <div>EMAIL</div>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <div>PASSWORD</div>
-          <ErrorMessage name="password" component="div" />
-          <a href="/#">FORGOT YOUR PASSWORD?</a>
-          <div>
-            <input type="checkbox" name="remember" />
-            Remember Me
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  );
-};
