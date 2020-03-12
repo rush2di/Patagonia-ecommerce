0a64f680a084ca93d48a8a3b1068a4615090172b
Rochdi <roch2di@gmail.com>
2020-03-12 18:18:14 +0100

    --Update-code-formating-&restyling

--- src/pages/products.js
+++ src/pages/products.js
@@ -35,22 +35,43 @@ const Products = () => {
   );
 };
 
-const ProductsList = ({ categories, filtred }) => {
+const ProductsList = ({ categories, filtred = false }) => {
   const { slugId } = useParams();
-
-  const renderLogic = !!filtred
+  const renderLogic = filtred
     ? categories.filter(item => item.id === slugId)
     : categories;
-  const productsMapper = renderLogic.map(category => {
+
+  return (
+    <div className="products--main container">
+      <AsideBox categories={categories} />
+      <div className="products--main-store">
+        <ProductsListCards renderLogic={renderLogic} />
+      </div>
+    </div>
+  );
+};
+
+const ProductsListCards = ({ renderLogic }) =>
+  renderLogic.map(category => {
     const { id, name, products } = category;
-    let productsDetails = products.map(item => (
-      <Link
-        to={{
-          pathname: `/shop/surfboard/${item.refrence}`,
-          state: item
-        }}
-        key={item.refrence}
-      >
+    return (
+      <div key={`listp${id}`} className="products--card-wrapper">
+        <h3>{name}</h3>
+        <div className="products--grid">
+          <ListCardsDetails products={products} />
+        </div>
+      </div>
+    );
+  });
+
+const ListCardsDetails = ({ products }) =>
+  products.map(item => {
+    const routerParams = {
+      pathname: `/shop/surfboard/${item.refrence}`,
+      state: item
+    };
+    return (
+      <Link to={routerParams} key={item.refrence}>
         <div className="products--item">
           <img src={item.image} alt="" />
           <div className="products--item-flex">
@@ -59,54 +80,11 @@ const ProductsList = ({ categories, filtred }) => {
           </div>
         </div>
       </Link>
-    ));
-    return (
-      <div key={`listp${id}`} className="products--card-wrapper">
-        <h3>{name}</h3>
-        <div className="products--grid">{productsDetails}</div>
-      </div>
     );
   });
-
-  return (
-    <div className="products--main container">
-      <AsideBox categories={categories} />
-      <div className="products--main-store">{productsMapper}</div>
-    </div>
-  );
-};
 
 const AsideBox = ({ categories }) => {
   const { slugId } = useParams();
-
-  const listMapper = categories.map(category => {
-    let { id, name, products } = category;
-
-    let sublistMapper = products.map((item, i) => {
-      let { refrence, prodName } = item;
-      return (
-        <Link
-          to={{
-            pathname: `/shop/surfboard/${refrence}`,
-            state: item
-          }}
-          key={`${refrence}${i}`}
-        >
-          <span>{prodName}</span>
-        </Link>
-      );
-    });
-
-    let sublistLogic = id === slugId && sublistMapper;
-
-    return (
-      <li key={`listc${id}`}>
-        <Link to={`/shop/surfboards/${id}`}>{name}</Link>
-        <div className="products--main-sublist">{sublistLogic}</div>
-      </li>
-    );
-  });
-
   return (
     <div className="products--main-aside">
       <h3>SHORTCUTS</h3>
@@ -114,7 +92,7 @@ const AsideBox = ({ categories }) => {
         <li>
           <Link to="/shop/surfboards/">ALL</Link>
         </li>
-        {listMapper}
+        <AsideBoxLists slug={slugId} categories={categories} />
       </ul>
       <p>
         Surfboards manufactured and sold by Patagonia are guaranteed for quality
@@ -125,5 +103,32 @@ const AsideBox = ({ categories }) => {
     </div>
   );
 };
+
+const AsideBoxLists = ({ categories, slug }) =>
+  categories.map(category => {
+    let { id, name, products } = category;
+    return (
+      <li key={`listc${id}`}>
+        <Link to={`/shop/surfboards/${id}`}>{name}</Link>
+        <div className="products--main-sublist">
+          {id === slug && <AsideBoxSublists products={products} />}
+        </div>
+      </li>
+    );
+  });
+
+const AsideBoxSublists = ({ products }) =>
+  products.map((item, i) => {
+    let { refrence, prodName } = item;
+    let routerParams = {
+      pathname: `/shop/surfboard/${refrence}`,
+      state: item
+    };
+    return (
+      <Link to={routerParams} key={`${refrence}${i}`}>
+        <span>{prodName}</span>
+      </Link>
+    );
+  });
 
 export default Products;
