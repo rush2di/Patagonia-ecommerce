4e10dbdada46aa18fb7707a2fa829b06d383b327
Rochdi <roch2di@gmail.com>
2020-03-12 23:10:05 +0100

    --Update-product-page

--- src/pages/product.js
+++ src/pages/product.js
@@ -4,14 +4,32 @@ import { Carousel } from "react-responsive-carousel";
 
 const Product = props => {
   const data = !!props.location.state && props.location.state;
+  const albumMapper = data.album.map((image, i) => {
+    return (
+      <div key={`carousel-img${i}`}>
+        <img className="carousel--image" src={image} alt="" />
+      </div>
+    );
+  });
   console.log(data);
   return (
     <div className="main--product">
-      <Carousel emulateTouch={true} autoPlay={true}>
-        <div>
-          <img className="carousel--image" src={data.image} alt="" />
-        </div>
-      </Carousel>
+      <div className="main--products-header">
+        <Carousel
+          className="container"
+          centerMode={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          emulateTouch={true}
+          autoPlay={true}
+        >
+          <div className="carousel--image-wrapper">
+            <img className="carousel--image" src={data.image} alt="" />
+          </div>
+          {!!data.album.length && albumMapper}
+        </Carousel>
+      </div>
     </div>
   );
 };
