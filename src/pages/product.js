4cda7cab47c7abb4367bb6d2273458a4701a9b77
Rochdi <roch2di@gmail.com>
2020-03-12 18:19:00 +0100

    --New-carousel-render

--- src/pages/product.js
+++ src/pages/product.js
@@ -5,7 +5,15 @@ import { Carousel } from "react-responsive-carousel";
 const Product = props => {
   const data = !!props.location.state && props.location.state;
   console.log(data);
-  return <div></div>;
+  return (
+    <div className="main--product">
+      <Carousel emulateTouch={true} autoPlay={true}>
+        <div>
+          <img className="carousel--image" src={data.image} alt="" />
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Product;
