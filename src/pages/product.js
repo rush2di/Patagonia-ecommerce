8c5eb009b9fdad40d575a2c5816a2d03be0acfad
Rochdi <roch2di@gmail.com>
2020-03-12 01:34:59 +0100

    --Init-carousel-component

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Product = props => {
   const data = !!props.location.state && props.location.state;
